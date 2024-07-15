05f9ab697f50c02bcb9c32136837cf8ccdbc0eca
aniketroy970 <aniket.roy@stu.adamasuniversity.ac.in>
2024-07-16 01:08:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,9 @@ app.listen(port,()=>{
     console.log(`Server run at port number ${port}`)
 })
 
-mongoose.connect('mongodb+srv://aniketroy:quizapp@cluster0.qovzcs3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Names',{
+mongoose.connect('mongodb+srv://aniketroy:quizapp@cluster0.qovzcs3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
     useNewUrlParser:true,
     useUnifiedTopology:true,
-    serverSelectionTimeoutMS: 30000, 
-  socketTimeoutMS: 45000, 
 }).then(()=>{
     console.log("MongoDB Connected.");
 })
